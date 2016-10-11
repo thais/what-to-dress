94bae8396ce7ddf21b7e3ec463d6f701ac77d8ad
Thais Hamilton <thamilto@thoughtworks.com>
2016-10-11 19:42:03 -0300

    Create a new lein project

---
+++ test/what_to_dress/core_test.clj
@@ -0,0 +1,7 @@
+(ns what-to-dress.core-test
+  (:require [clojure.test :refer :all]
+            [what-to-dress.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
